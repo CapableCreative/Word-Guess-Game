16768fc744dfa433e10d33b49af8ebec1a83b1a1
Stephen Fox <stephen@capablecreative.com>
2019-08-10 08:55:07 -0500

    Incremental commit

--- assets/js/wordgame.js
+++ assets/js/wordgame.js
@@ -11,47 +11,61 @@
         b. No Match == reduce number of guesses? or infinite guesses? & Display message of DOOM & store guess in GRAVEYARD
 
     // VARIABLE BANK
-        WORDBANK = array of words
-        CHOSEN = current word
-        KEYSTROKE = user's guess
-        WORDLETTER = individual letters in WORDBANK index item
-        MATCHED = successfully matched letters
-        GRAVEYARD = unmatched guesses so far
+        wordBank = array of words
+        chosen = current word
+        userGuess = user's guess
+        wordletter = individual letters in WORDBANK index item
+        matched = successfully matched letters
+        graveyard = unmatched guesses so far
 
 -------------------------------------------- */
 // JQuery function that allows js to start its work once the page is fully rendered.
 // NOTE: Setting scripts to load via "defer" also accomplishes this. - SLF
-
 $(document).ready(function() {
 
 // 1. Create a WORDBANK array to store possible words.
     var wordBank = ["terror","nightmare","horror","shreik","manbuns","decaf","midnight","headstone","cthulu","wraith","bloodbath"];
-    
+    var underline = ['_ '];
+
     // a. Display randomly CHOSEN index within WORDBANK
     var chosen = wordBank[Math.floor(Math.random() * wordBank.length)];
-    var gamers = document.getElementById('gameContent');
-    var dhold = document.getElementById('blankspace');
+    var gameHold = document.getElementById('gameContent');
+    var blankHold = document.getElementById('blankspace');
 
     var node = document.createElement("P");                 
     var textnode = document.createTextNode(chosen);         
     node.appendChild(textnode);                              
-    gamers.appendChild(node); 
+    gameHold.appendChild(node); 
 
     // b. Display individual letters of WORDBANK index, assign to WORDLETTER 
     var wordletter = chosen.length;
-    //var underline = "_ "
-
+    // c. Display individual underlines matching WORDLETTER 
     for (var i = 0; i < wordletter; i++) {
-        var underline = ['_ '];
-
         var pwrapper = document.createElement("P");
         var dashString = document.createTextNode(underline);
         pwrapper.appendChild(dashString);
-        dhold.appendChild(pwrapper);
-        
-        console.log(wordletter);
-
-       
+        blankHold.appendChild(pwrapper);  
     }
-    
+// 2. Account for score, remaining letters, and increments
+    var lettersLeft = chosen.length;
+
+    while (lettersLeft > 0) {
+        
+        var userGuess = prompt("Care to venture a letter guess?").toLowerCase();
+        if (userGuess == null) {
+            alert("Try again, chump. Invalid entry.")
+        }
+        else if (userGuess.length !== 1) {
+            alert("Pick ONE letter; no more, no less.")
+        }
+        else {
+            for (var j = 0; j < chosen.length; j++) {
+                if (chosen[j] == userGuess) {
+                    wordBank[j] = userGuess;
+                    lettersLeft--;
+                    alert('Good Job ' + userGuess + " is in the word!");
+                }     
+            }
+        }
+    }
 });
