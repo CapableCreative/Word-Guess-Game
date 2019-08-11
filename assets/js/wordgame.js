e93d3b6041d4be0d8a16b734373273718fce504a
Stephen Fox <stephen@capablecreative.com>
2019-08-11 13:40:40 -0500

    Commiting to test JQuery delay in live environment

--- assets/js/wordgame.js
+++ assets/js/wordgame.js
@@ -21,22 +21,18 @@
 -------------------------------------------- */
 // JQuery function that allows js to start its work once the page is fully rendered.
 // NOTE: Setting scripts to load via "defer" also accomplishes this. - SLF
-$(document).ready(function() {
+
+       
+
+
+
 
 // 1. Create a WORDBANK array to store possible words.
     var wordBank = ["terror","nightmare","horror","shreik","manbuns","decaf","midnight","headstone","cthulu","wraith","bloodbath"];
-    //var underline = ['_ '];
-    
 
     // a. Display randomly CHOSEN index within WORDBANK
     var chosen = wordBank[Math.floor(Math.random() * wordBank.length)];
-    // var gameHold = document.getElementById('gameContent');
     var blankHold = document.getElementById('blankspace');
-
-    //var node = document.createElement("P");                 
-    //var textnode = document.createTextNode(chosen);         
-    // node.appendChild(textnode);                              
-    // gameHold.appendChild(node); 
 
     // b. Display individual letters of WORDBANK index, assign to WORDLETTER 
     var wordletter = chosen.length;
@@ -44,7 +40,6 @@ $(document).ready(function() {
     for (var i = 0; i < wordletter; i++) {
         var pwrapper = document.createElement("P");
         var matchwrapper = document.createElement("SPAN");
-        //var dashString = document.createTextNode(chosen[i]);
         var matchedLetter = document.createTextNode(chosen[i]);
         pwrapper.appendChild(matchedLetter);
         blankHold.appendChild(matchwrapper);  
@@ -53,29 +48,58 @@ $(document).ready(function() {
     }
 // 2. Account for score, remaining letters, and increments
     var lettersLeft = chosen.length;
-
+    var userGuess = 'No Letter Assigned Yet';
     while (lettersLeft > 0) {      
-
-        var userGuess = prompt("Care to venture a letter guess?").toLowerCase();
-        if (userGuess == null) {
-            console.log("The entered value in null")
-        }
-        else if (userGuess.length > 1) {
-            alert("Pick ONE letter; no more, no less.")
-        }
-        else {
-            for (var j = 0; j < chosen.length; j++) {
-                var successP = document.createElement('P');
-                var successText = document.createTextNode(chosen[j]);
-                var messageDiv = document.getElementById('messageToPlayer');
-                if (chosen[j] == userGuess) {
-                    wordBank[j] = userGuess;
-                    lettersLeft--;
-                    messageDiv.appendChild(successP);
-                    successP.appendChild(successText);
-                    document.getElementsByTagName('span')[j].style.color = 'RED';
-                } 
+        $(document).keyup(function(userGuess){
+            switch(userGuess.keyCode){
+                case 65	:	userGuess = 'a';	break;	// a
+                case 66	:	userGuess = 'b';	break;	// b
+                case 67	:	userGuess = 'c';	break;	// c
+                case 68	:	userGuess = 'd';	break;	// d
+                case 69	:	userGuess = 'e';	break;	// e
+                case 70	:	userGuess = 'f';	break;	// f
+                case 71	:	userGuess = 'g';	break;	// g
+                case 72	:	userGuess = 'h';	break;	// h
+                case 73	:	userGuess = 'i';	break;	// i
+                case 74	:	userGuess = 'j';	break;	// j
+                case 75	:	userGuess = 'k';	break;	// k
+                case 76	:	userGuess = 'l';	break;	// l
+                case 77	:	userGuess = 'm';	break;	// m
+                case 78	:	userGuess = 'n';	break;	// n
+                case 79	:	userGuess = 'o';	break;	// o
+                case 80	:	userGuess = 'p';	break;	// p
+                case 81	:	userGuess = 'q';	break;	// q
+                case 82	:	userGuess = 'r'; 	break;	// r
+                case 83	:	userGuess = 's';	break;	// s
+                case 84	:	userGuess = 't';	break;	// t
+                case 85	:	userGuess = 'u';	break;	// u
+                case 86	:	userGuess = 'v';	break;	// v
+                case 87	:	userGuess = 'w';	break;	// w
+                case 88	:	userGuess = 'x';	break;	// x
+                case 89	:	userGuess = 'y';	break;	// y
+                case 90	:	userGuess = 'z';	break;	// z
             }
+            console.log('User Guess is' + userGuess);
+            if (userGuess == null) {
+                console.log("The entered value in null")
+            }
+            else if (userGuess.length > 1) {
+                alert("Pick ONE letter; no more, no less.")
+            }
+            else {
+                for (var j = 0; j < chosen.length; j++) {
+                    var successP = document.createElement('P');
+                    var successText = document.createTextNode(chosen[j]);
+                    var messageDiv = document.getElementById('messageToPlayer');
+                    if (chosen[j] == userGuess) {
+                        wordBank[j] = userGuess;
+                        lettersLeft--;
+                        messageDiv.appendChild(successP);
+                        successP.appendChild(successText);
+                        document.getElementsByTagName('span')[j].style.color = 'RED';
+                    } 
+                }
+            }
+        });
+
         }
-    }
-});
